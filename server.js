87ee3b0c3097a0a010732d3664eab49466edbc20
DavidWei95036593 <dw36593@pausd.us>
2018-04-27 13:31:43 -0700

    we can mount stuff yeah!

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require(`path`);
 const bodyParser = require(`body-parser`);
 const exphbs = require(`express-handlebars`);
 const mongoose = require(`mongoose`);
+const users = require(`./routes/users.js`);
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -13,13 +14,18 @@ app.engine(`hbs`,exphbs({defaultLayout:`main`,
                          extname:      `.hbs`}))
 app.set(`view engine`,`hbs`);
 
+//mount routes
+app.use(`/users`,users);
+
+
+
 app.get(`/`,(req,res)=>{
   res.render(`index`);
 })
 
-app.get(`/register`,(req,res)=>{
-  res.render(`register`);
-})
+// app.get(`/register`,(req,res)=>{
+//   res.render(`register`);
+// })
 
 app.listen(port,()=>{
   console.log(`Web Server up on port ${port}!`);
