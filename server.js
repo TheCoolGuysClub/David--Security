b139e626d333a83c7f60afe2ef1ee056195f14bc
DavidWei95036593 <dw36593@pausd.us>
2018-04-27 13:22:47 -0700

    the layout is working

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const mongoose = require(`mongoose`);
 const port = process.env.PORT || 3000;
 const app = express();
 
+//view engine setup
+app.set(`views`,path.join(__dirname,`views`));
+app.engine(`hbs`,exphbs({defaultLayout:`main`,
+                         extname:      `.hbs`}))
+app.set(`view engine`,`hbs`);
+
+app.get(`/`,(req,res)=>{
+  res.render(`index`);
+})
+
+app.get(`/register`,(req,res)=>{
+  res.render(`register`);
+})
 
 app.listen(port,()=>{
   console.log(`Web Server up on port ${port}!`);
