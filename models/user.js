64f3fb11038084b1ea89e16ab24f6fbf641b7a5e
DavidWei95036593 <dw36593@pausd.us>
2018-04-30 13:00:15 -0700

    add valiadotr

--- models/user.js
+++ models/user.js
@@ -1,2 +1,17 @@
 const mongoose = require(`mongoose`);
 const bcrypt = require(`bcryptjs`);
+const validator = require(`validator`)
+
+const userSchema = mongoose.Schema({
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  }
+  password:{
+    type:String,
+    require:true,
+    minLength:6
+  }
+
+});
