089b1b8d26a8443b113e5547c87ded6ccb0300c0
DavidWei95036593 <dw36593@pausd.us>
2018-04-30 13:07:39 -0700

    validator

--- models/user.js
+++ models/user.js
@@ -6,7 +6,16 @@ const userSchema = mongoose.Schema({
   email:{
     type:String,
     required:true,
-    unique:true
+    unique:true,
+    //if we want more than one validator, we have to right more validate
+    validate:{
+      //validator.isEmail is a method inside validator that checks
+      //if the String is in email format
+      validator:validator.isEmail,
+      //message is the message that would appeared if the validator failed
+      //to validate
+      message:`{VALUE} is not an email`
+    }
   }
   password:{
     type:String,
