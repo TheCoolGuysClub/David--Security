2f87294faf0d3b63162f968c15cc1a174b1e4b60
DavidWei95036593 <dw36593@pausd.us>
2018-04-30 15:50:46 -0700

    didn't finsih

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,16 @@ const express = require(`express`);
 const users = express.Router();
 const User = require(`../models/user.js`);
 
+const logger = (req,res,next){
+  console.log(req.body);
+  next();   
+}
+
 users.get(`/register`,(req,res)=>{
   res.render(`register`);
 })
 
-users.post(`/register`,(req,res)=>{
+users.post(`/register`,logger,(req,res)=>{
   console.log(req.body);
 })
 
